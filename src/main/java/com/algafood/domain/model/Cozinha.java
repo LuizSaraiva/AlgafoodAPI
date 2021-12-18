0bd32519b0e1388b7952e2161cb3c46ac2cd25f6
LuizSaraiva <l.henrique.saraiva@gmail.com>
2021-12-18 15:08:47 -0300

    relacionamento OneToMany

--- src/main/java/com/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algafood/domain/model/Cozinha.java
@@ -1,9 +1,14 @@
 package com.algafood.domain.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,4 +24,8 @@ public class Cozinha {
 	private Long id;
 
 	private String nome;
+	
+	@OneToMany(mappedBy = "cozinha")
+	@JsonIgnore
+	private List<Restaurante> restaurantes;
 }
--- src/main/java/com/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algafood/domain/model/Restaurante.java
@@ -27,5 +27,4 @@ public class Restaurante {
 	@ManyToOne
 	private Cozinha cozinha;
 	
-	
 }
