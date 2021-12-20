adebd1517f825d36aead8791b270d2c2d9908ed2
LuizSaraiva <l.henrique.saraiva@gmail.com>
2021-12-20 19:40:37 -0300

    criada entidade produto

---
+++ src/main/java/com/algafood/domain/model/Produto.java
@@ -0,0 +1,28 @@
+package com.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Produto {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+	private Boolean ativo;
+
+}
