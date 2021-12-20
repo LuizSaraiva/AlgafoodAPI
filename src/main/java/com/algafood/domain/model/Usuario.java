1d74d4f1a2b0697ff172c78b6206ce6e9ba1069b
LuizSaraiva <l.henrique.saraiva@gmail.com>
2021-12-20 20:25:50 -0300

    vinculado usuario a grupo

--- src/main/java/com/algafood/domain/model/Usuario.java
+++ src/main/java/com/algafood/domain/model/Usuario.java
@@ -1,12 +1,16 @@
 package com.algafood.domain.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -23,11 +27,23 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@JoinColumn(nullable = false)
 	private String nome;
+
+	@JoinColumn(nullable = false)
 	private String email;
+
+	@JoinColumn(nullable = false)
 	private String senha;
 	
 	@CreationTimestamp
 	@JoinColumn(nullable = false)
 	private LocalDateTime dataCadastro;
+
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo",
+		joinColumns = @JoinColumn(name = "usuario_id"),
+		inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos = new ArrayList<>();
+	
 }
