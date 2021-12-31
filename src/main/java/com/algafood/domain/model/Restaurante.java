be14ea1a964d746bbd202704c90a961ea6cca070
LuizSaraiva <l.henrique.saraiva@gmail.com>
2021-12-31 11:33:58 -0300

    alterada estrategia eager para lazy

--- src/main/java/com/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algafood/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -47,8 +48,9 @@ public class Restaurante {
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
-	
-	@ManyToOne
+
+	@JsonIgnore
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Cozinha cozinha;
 	
 	@JsonIgnore
