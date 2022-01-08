c3e1d3473d68d5389ad72d9763ec3dc7515d84ee
LuizSaraiva <l.henrique.saraiva@gmail.com>
2022-01-08 18:10:00 -0300

    exception entidadenaoencontrada customizada

--- src/main/java/com/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,15 +1,19 @@
 package com.algafood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
 
 	private static final long serialVersionUID = 1L;
 	
+	public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+		super(status, mensagem);
+	}
+
 	public EntidadeNaoEncontradaException(String mensagem) {
-		super(mensagem);
+		super(HttpStatus.NOT_FOUND, mensagem);
 	}
 
 }
