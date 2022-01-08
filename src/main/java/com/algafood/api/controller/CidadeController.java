e422174de5bd1b30506de34965bdf78ef3e28c4e
LuizSaraiva <l.henrique.saraiva@gmail.com>
2022-01-08 19:30:58 -0300

    adicionado recurso delete para cidade

--- src/main/java/com/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algafood/api/controller/CidadeController.java
@@ -1,16 +1,17 @@
 package com.algafood.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algafood.domain.exception.EntidadeNaoEncontradaException;
@@ -47,5 +48,11 @@ public class CidadeController {
 		}catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+	
+	@DeleteMapping("/{idCidade}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(Long idCidade) {
+		service.remover(idCidade);
 	}
 }
